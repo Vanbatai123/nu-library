05b4b2a8a30bfdcbd6152a4f9faa221ad976d2d8
Tim Bergling <timbergling65535@gmail.com>
2025-04-17 20:34:43 +0700

    [MCU8BIT][NUVOTON] update note

--- lib/N76_uart0/src/N76_uart0.c
+++ lib/N76_uart0/src/N76_uart0.c
@@ -24,8 +24,8 @@ void UART0_begin(uint8_t baud)
     P07_Quasi_Mode; // Setting UART pin as Quasi mode for transmit
 
     TH1 = baud;   // set baudrate
-    SCON = 0x50;  // UART0 Mode1,REN=1,TI=1
-    TMOD |= 0x20; // Timer1 Mode1
+    SCON = 0x50;  // UART0 Mode1,REN=1,TI=1 
+    TMOD |= 0x20; // Timer1 Mode1 
     set_SMOD;     // UART0 Double Rate Enable
     set_T1M;
     clr_BRCK; // Serial port 0 baud rate clock source = Timer1
@@ -57,7 +57,7 @@ void UART0_println(char *str)
 void UART0_printNum(int32_t num, uint8_t base)
 {
     uint8_x dis[20];          // array of converted number
-    int8_t max = 0, flag = 0; // max: index of dis array, flag: = 1 if negative
+    int8_t max = 0, negFlag = false; // max: index of dis array, negFlag: = 1 if negative
 
     if (num == 0) // input 0
     {
@@ -66,14 +66,14 @@ void UART0_printNum(int32_t num, uint8_t base)
     else if (num < 0) // negative number
     {
         num = 0 - num;
-        flag = 1;
+        negFlag = true;
     }
     while (num > 0) // convert to base number and add to dis array
     {
         if (num % base >= 10)
-            dis[max] = num % base + 55;
+            dis[max] = num % base + 55; //
         else
-            dis[max] = num % base + 48;
+            dis[max] = num % base + 48; //
 
         num = num / base;
         max++;
@@ -93,7 +93,7 @@ void UART0_printNum(int32_t num, uint8_t base)
         dis[max++] = '0';
     }
 
-    if (flag == 1) // add minus to negative number
+    if (negFlag == true) // add minus to negative number
         dis[max++] = '-';
 
     for (uint8_t i = 0; i < max / 2; i++) // revert dis array
